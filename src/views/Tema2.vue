47b27d1b2363465e482452b67a64019603023d1f
Va-Gomez <99420453+Va-Gomez@users.noreply.github.com>
2022-03-15 01:32:39 -0500

    tema2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,9 +7,19 @@
       .titulo-principal__numero
         span.text-white 1
       h1 Automatización de procesos informáticos
-    
+    p Para el desarrollo e implementación se utilizará la herramienta UiPath, la cual incluye desarrollo de aplicaciones sin programación, es decir lo puede hacer una persona sin amplios conocimientos de programación, tiene reconocimiento óptico de caracteres (OCR), inteligencia artificial (IA) y aprendizaje automático. Se puede complementar de igual forma con diferentes tipos de programación.  La siguiente imagen muestra una vista del sitio web de UiPath:
+    br
+    br
+    .titulo-sexto.color-acento-contenido.pos13
+      h5 Página de Inicio de UiPath
 
+    figure.col-10.pos12
+      .video
+        iframe(src="https://www.uipath.com" allowtransparency="true" frameborder="0" scrolling="no" width="640" height="390")
 
+    .titulo-segundo.color-acento-contenido(data-aos="zoom-in").pos5.col-12
+      h2(data-aos="zoom-in") 2.1 Instalación de UiPath
+    p Los pasos generales para la descarga de UiPath son los siguientes:
 
 </template>
 
